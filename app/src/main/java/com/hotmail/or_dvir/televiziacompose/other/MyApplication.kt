ba15b3c66f771689f66ac4b20ed1b1fa5b80bb9e
Or Dvir <or_dvir@hotmail.com>
2021-09-05 12:24:28 +0200

    added custom application class

---
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/other/MyApplication.kt
@@ -0,0 +1,32 @@
+package com.hotmail.or_dvir.televiziacompose.other
+
+import android.app.Application
+import com.hotmail.or_dvir.televiziacompose.repositories.MoviesRepository
+import com.hotmail.or_dvir.televiziacompose.repositories.MoviesRepositoryImpl
+import com.hotmail.or_dvir.televiziacompose.ui.login_register.LoginViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.module
+
+class MyApplication : Application()
+{
+    private val appModule = module {
+        viewModel { LoginViewModel(androidApplication()) }
+        factory<MoviesRepository> { MoviesRepositoryImpl() }
+    }
+
+    override fun onCreate()
+    {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@MyApplication)
+            modules(appModule)
+        }
+    }
+}
