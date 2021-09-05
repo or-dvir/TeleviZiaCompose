5402b831d6e24c10f39c1b232c37dd9fff645c38
Or Dvir <or_dvir@hotmail.com>
2021-09-05 18:58:55 +0200

    Create TextFieldState.kt

---
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/TextFieldState.kt
@@ -0,0 +1,17 @@
+package com.hotmail.or_dvir.televiziacompose.ui.login_register
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+open class TextFieldState(
+    text: String,
+    error: String,
+)
+{
+    var text by mutableStateOf(text)
+    var error by mutableStateOf(error)
+
+    fun hasText() = text.isBlank()
+    fun hasError() = error.isBlank()
+}
