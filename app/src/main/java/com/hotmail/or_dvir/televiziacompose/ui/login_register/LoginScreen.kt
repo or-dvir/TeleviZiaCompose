21e8975856f5fc1d94153a32d89bb55b2ef13142
Or Dvir <or_dvir@hotmail.com>
2021-09-05 16:43:05 +0200

    added error message to email field

--- app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginScreen.kt
@@ -2,7 +2,10 @@ package com.hotmail.or_dvir.televiziacompose.ui.login_register
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hotmail.or_dvir.televiziacompose.R
@@ -20,67 +24,77 @@ import com.hotmail.or_dvir.televiziacompose.ui.theme.TeleviZiaComposeTheme
 @Composable
 fun LoginScreen()
 {
+    //todo look into landscape mode
     TeleviZiaComposeTheme {
-        UserNamePasswordSubmit()
+        //todo logo (and app name???)
+        UserInput()
     }
+
     //todo add ALL composables here
 }
 
-@Preview(
-//    showSystemUi = true,
-    showBackground = true
+@Composable
+fun EmailField(
+    text: String = "",
+    error: String = "",
+    onTextChanged: (String) -> Unit
 )
-@Composable
-fun LoginScreenPreview()
 {
-    LoginScreen()
-}
+    //todo make a general class for all TextFields
+    // should hold text, error status/message
+    val showError = error.isNotBlank()
 
-@Preview(showBackground = true)
-@Composable
-fun EmailFieldPreview()
-{
-    EmailField("my@email.com")
-}
+    Column {
+        //todo
+        // add ime action "next" to move to password field
+        // handle error
+        //      invalid email
+        OutlinedTextField(
+            value = text,
+            modifier = Modifier.fillMaxWidth(),
+            isError = showError,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            onValueChange = { onTextChanged(it) },
+            label = { Text(text = stringResource(id = R.string.hint_email)) }
+        )
 
-@Preview(
-    showBackground = true
-)
-@Composable
-fun EmailFieldPreview_longText()
-{
-    EmailField("a very long text a very long text a very long text a very long text a very long text a very long text a very long text ")
-}
-
-@Composable
-fun EmailField(text: String = "")
-{
-    var emailState by remember { mutableStateOf(text) }
-    //todo
-    // add ime action "next" to move to password field
-    // invalid email handling
-    // look into landscape mode
-    OutlinedTextField(
-        modifier = Modifier.fillMaxWidth(),
-        singleLine = true,
-        value = emailState,
-        onValueChange = { emailState = it },
-        label = {
-            Text(text = stringResource(id = R.string.hint_email))
+        if (showError)
+        {
+            Text(
+                modifier = Modifier.offset(16.dp, 0.dp),
+                style = MaterialTheme.typography.caption,
+                text = error,
+                color = MaterialTheme.colors.error
+            )
         }
-    )
+    }
 }
 
 @Composable
-fun UserNamePasswordSubmit()
+fun UserInput()
 {
-    //todo should be stateful or stateless?
-    // are other components interested? YES! WE NEED THE INPUT! -> stateless
+    //todo do i need to remember the focused field?
 
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
-        EmailField()
+        var email by remember { mutableStateOf("") }
+        var error by remember { mutableStateOf("") }
+
+        EmailField(
+            text = email,
+            error = error
+        ) {
+            email = it
+            error = if (email.length > 3)
+            {
+                "max 3 characters"
+            } else
+            {
+                ""
+            }
+        }
 
         //todo password
         // hide characters by default
@@ -91,13 +105,18 @@ fun UserNamePasswordSubmit()
     }
 }
 
-//todo taking up WAY too much computing power (refreshes on every change?)
-// enable later!!!
-//@Preview
-//@Composable
-//fun LoginPreview()
-//{
-//    TeleviZiaComposeTheme {
-//        LoginScreen()
-//    }
-//}
+//region
+@Preview(showSystemUi = true)
+@Composable
+fun LoginScreenPreview()
+{
+    LoginScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmailErrorPreview()
+{
+    EmailField(error = "some error") {}
+}
+//endregion
