07422702503b87407dd5c2575d8f97b1bf032f74
Or Dvir <or_dvir@hotmail.com>
2021-09-14 00:31:59 +0200

    added input validation and state of the login process (not finished)

--- app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginViewModel.kt
@@ -5,9 +5,13 @@ import androidx.core.util.PatternsCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.hotmail.or_dvir.televiziacompose.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class LoginViewModel(app: Application) : AndroidViewModel(app)
+class LoginViewModel(private val app: Application) : AndroidViewModel(app)
 {
     private val mainDispatcher = Dispatchers.Main
 
@@ -15,6 +19,10 @@ class LoginViewModel(app: Application) : AndroidViewModel(app)
     val emailState: LiveData<TextFieldState> = _emailState
     private val _passwordState = MutableLiveData(TextFieldState())
     val passwordState: LiveData<TextFieldState> = _passwordState
+
+    private val _loginState = MutableLiveData(LoginState.Idle)
+    observe this in the login screen!!!
+    val loginState: LiveData<LoginState> = _loginState
 
     fun onEmailInputChanged(newInput: String)
     {
@@ -26,29 +34,56 @@ class LoginViewModel(app: Application) : AndroidViewModel(app)
         _passwordState.value = _passwordState.value!!.copy(text = newInput)
     }
 
-    //todo
-    // hold and update email state
-    // hold and update password state
+    fun login()
+    {
+        if (validateEmail() && validatePassword())
+        {
+            //valid input. perform login
+            viewModelScope.launch(mainDispatcher) {
+                _loginState.value = LoginState.Loading
+                //todo implement actual login functionality
+                // add error state with message (user doesnt exist, wrong password)
+                //      add user functionality to database (including simulated failure
+                delay(3000)
+                _loginState.value = LoginState.Success
+            }
+        }
+    }
 
-    /**
-     * @return String error message, or `null` if success
-     */
-    //todo fix me
-//    fun login(email: String, password: String): String?
-//    {
-//        add input validation(isEmailValid, no empty fields, what else?)
-//        viewModelScope.launch(mainDispatcher) {
-//            return if (isEmailValid(email))
-//            {
-//                //todo implement actual login functionality
-//                delay(3000)
-//                null
-//            } else
-//            {
-//                app.getString(R.string.error_invalidEmail)
-//            }
-//        }
-//    }
+    private fun validatePassword(): Boolean
+    {
+        val isValid = _passwordState.value?.hasText() ?: false
 
-    private fun isEmailValid(email: String) = PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
+        if (!isValid)
+        {
+            _passwordState.value = _passwordState.value!!.copy(
+                error = app.getString(R.string.error_emptyField)
+            )
+        }
+
+        return isValid
+    }
+
+    private fun validateEmail(): Boolean
+    {
+        val isValid = _emailState.value?.text?.let {
+            PatternsCompat.EMAIL_ADDRESS.matcher(it).matches()
+        } ?: false
+
+        if (!isValid)
+        {
+            _emailState.value = _emailState.value!!.copy(
+                error = app.getString(R.string.error_invalidEmail)
+            )
+        }
+
+        return isValid
+    }
+}
+
+enum class LoginState
+{
+    Idle,
+    Loading,
+    Success
 }
