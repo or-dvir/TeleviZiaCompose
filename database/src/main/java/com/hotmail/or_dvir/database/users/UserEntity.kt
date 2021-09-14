acaa5cdfd904872ff17ab99850d85715c426d8de
Or Dvir <or_dvir@hotmail.com>
2021-09-14 12:33:25 +0200

    created users entity and data source

---
+++ database/src/main/java/com/hotmail/or_dvir/database/users/UserEntity.kt
@@ -0,0 +1,6 @@
+package com.hotmail.or_dvir.database.users
+
+data class UserEntity(
+    val email: String,
+    val password: String
+)
---
+++ database/src/main/java/com/hotmail/or_dvir/database/users/UsersDataSource.kt
@@ -0,0 +1,50 @@
+package com.hotmail.or_dvir.database.users
+
+import kotlin.random.Random
+
+object UsersDataSource
+{
+    private val allUsers = listOf<UserEntity>()
+
+    /**
+     * @return UserError? an error, or null if successful
+     */
+    fun register(user: UserEntity): UserError?
+    {
+        //todo
+        return null
+    }
+
+    /**
+     * @return UserError? an error, or null if successful
+     */
+    fun login(email: String, password: String): UserError?
+    {
+        val dbUser = allUsers.find { it.email == email } ?: return UserError.NonExistingUser
+
+        if (dbUser.password != password)
+        {
+            return UserError.WrongPassword
+        }
+
+        return if (Random.nextInt(100) <= 80)
+        {
+            null
+        } else
+        {
+            UserError.ConnectionError
+        }
+    }
+
+    ////////////////////////////
+    ////////////////////////////
+    ////////////////////////////
+    ////////////////////////////
+
+    sealed class UserError
+    {
+        object NonExistingUser : UserError()
+        object WrongPassword : UserError()
+        object ConnectionError : UserError()
+    }
+}
