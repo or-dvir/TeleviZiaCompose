81f07b00217ed24d2659a92ce103a76730d87b1d
Or Dvir <or_dvir@hotmail.com>
2021-09-14 11:13:55 +0200

    created a single ui state

--- app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginViewModel.kt
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginViewModel.kt
@@ -5,85 +5,86 @@ import androidx.core.util.PatternsCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
 import com.hotmail.or_dvir.televiziacompose.R
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class LoginViewModel(private val app: Application) : AndroidViewModel(app)
 {
     private val mainDispatcher = Dispatchers.Main
 
-    private val _emailState = MutableLiveData(TextFieldState())
-    val emailState: LiveData<TextFieldState> = _emailState
-    private val _passwordState = MutableLiveData(TextFieldState())
-    val passwordState: LiveData<TextFieldState> = _passwordState
-
-    private val _loginState = MutableLiveData(LoginState.Idle)
-    observe this in the login screen!!!
-    val loginState: LiveData<LoginState> = _loginState
+    private val _uiState = MutableLiveData(LoginUiState())
+    val uiState: LiveData<LoginUiState> = _uiState
 
     fun onEmailInputChanged(newInput: String)
     {
-        _emailState.value = _emailState.value!!.copy(text = newInput)
+        _uiState.value = uiState.value!!.copy(emailText = newInput)
     }
 
     fun onPasswordInputChanged(newInput: String)
     {
-        _passwordState.value = _passwordState.value!!.copy(text = newInput)
+        _uiState.value = uiState.value!!.copy(passwordText = newInput)
     }
 
-    fun login()
-    {
-        if (validateEmail() && validatePassword())
-        {
-            //valid input. perform login
-            viewModelScope.launch(mainDispatcher) {
-                _loginState.value = LoginState.Loading
-                //todo implement actual login functionality
-                // add error state with message (user doesnt exist, wrong password)
-                //      add user functionality to database (including simulated failure
-                delay(3000)
-                _loginState.value = LoginState.Success
-            }
-        }
-    }
+//    fun login()
+//    {
+//        if (validateEmail() && validatePassword())
+//        {
+//            //valid input. perform login
+//            viewModelScope.launch(mainDispatcher) {
+//                _loginState.value = LoginState.Loading
+//                //todo implement actual login functionality
+//                // add error state with message (user doesnt exist, wrong password)
+//                //      add user functionality to database (including simulated failure
+//                delay(3000)
+//                _loginState.value = LoginState.Success
+//            }
+//        }
+//    }
 
     private fun validatePassword(): Boolean
     {
-        val isValid = _passwordState.value?.hasText() ?: false
+        _uiState.value!!.apply {
+            val isPasswordEmpty = passwordText.isBlank()
+            if (isPasswordEmpty)
+            {
+                _uiState.value = _uiState.value!!.copy(
+                    passwordError = app.getString(R.string.error_emptyField)
+                )
 
-        if (!isValid)
-        {
-            _passwordState.value = _passwordState.value!!.copy(
-                error = app.getString(R.string.error_emptyField)
-            )
+                return false
+            }
+
+            return true
         }
-
-        return isValid
     }
 
     private fun validateEmail(): Boolean
     {
-        val isValid = _emailState.value?.text?.let {
-            PatternsCompat.EMAIL_ADDRESS.matcher(it).matches()
-        } ?: false
+        _uiState.value!!.apply {
+            val isEmailValid = PatternsCompat.EMAIL_ADDRESS.matcher(emailText).matches()
+            if (!isEmailValid)
+            {
+                _uiState.value = _uiState.value!!.copy(
+                    emailError = app.getString(R.string.error_invalidEmail)
+                )
 
-        if (!isValid)
-        {
-            _emailState.value = _emailState.value!!.copy(
-                error = app.getString(R.string.error_invalidEmail)
-            )
+                return false
+            }
+
+            return true
         }
-
-        return isValid
     }
-}
 
-enum class LoginState
-{
-    Idle,
-    Loading,
-    Success
+    ////////////////////////////////
+    ////////////////////////////////
+    ////////////////////////////////
+    ////////////////////////////////
+
+    data class LoginUiState(
+        val emailText: String = "",
+        val emailError: String = "",
+        val passwordText: String = "",
+        val passwordError: String = "",
+        val isLoading: Boolean = false
+    )
 }
