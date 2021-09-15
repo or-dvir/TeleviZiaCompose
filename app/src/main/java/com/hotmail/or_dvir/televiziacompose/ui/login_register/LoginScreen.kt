055973aaa433fbe6f55cccad489c0dce441d1ae1
Or Dvir <or_dvir@hotmail.com>
2021-09-15 15:34:05 +0200

    extracted shared composables

--- app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/login_register/LoginScreen.kt
@@ -1,7 +1,5 @@
 package com.hotmail.or_dvir.televiziacompose.ui.login_register
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -12,36 +10,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hotmail.or_dvir.televiziacompose.R
 import com.hotmail.or_dvir.televiziacompose.ui.login_register.LoginViewModel.LoginUiState
+import com.hotmail.or_dvir.televiziacompose.ui.shared.OutlinesTextFieldWithError
+import com.hotmail.or_dvir.televiziacompose.ui.shared.PasswordTextField
 import com.hotmail.or_dvir.televiziacompose.ui.theme.TeleviZiaComposeTheme
 
 @Composable
@@ -110,93 +98,6 @@ fun LoadingIndicatorFullScreen(modifier: Modifier = Modifier)
 }
 
 @Composable
-fun PasswordField(
-    state: LoginUiState,
-    modifier: Modifier = Modifier,
-    onTextChanged: (String) -> Unit
-)
-{
-    //todo this will not keep for configuration changes
-    var isPasswordVisible by remember { mutableStateOf(false) }
-
-    val passwordTransformation: VisualTransformation
-    @DrawableRes val passwordIconId: Int
-    @StringRes val passwordContentDescriptionId: Int
-
-    if (isPasswordVisible)
-    {
-        passwordIconId = R.drawable.ic_visibility_off
-        passwordContentDescriptionId = R.string.contentDescription_hidePassword
-        passwordTransformation = VisualTransformation.None
-    } else
-    {
-        //password is hidden
-        passwordIconId = R.drawable.ic_visibility_on
-        passwordContentDescriptionId = R.string.contentDescription_showPassword
-        passwordTransformation = PasswordVisualTransformation()
-    }
-
-    EmailPasswordTextField(
-        text = state.passwordText,
-        error = state.passwordError,
-        hint = R.string.hint_password,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = passwordTransformation,
-        modifier = modifier,
-        onTextChanged = onTextChanged,
-        trailingIcon = {
-            IconButton(
-                onClick = { isPasswordVisible = !isPasswordVisible }
-            ) {
-                Icon(
-                    painter = painterResource(passwordIconId),
-                    contentDescription = stringResource(passwordContentDescriptionId)
-                )
-            }
-        }
-    )
-}
-
-@Composable
-fun EmailPasswordTextField(
-    text: String,
-    error: String,
-    @StringRes hint: Int,
-    keyboardOptions: KeyboardOptions,
-    visualTransformation: VisualTransformation,
-    modifier: Modifier,
-    trailingIcon: @Composable (() -> Unit)?,
-    onTextChanged: (String) -> Unit
-)
-{
-    val showError = error.isNotBlank()
-
-    Column {
-        OutlinedTextField(
-            visualTransformation = visualTransformation,
-            value = text,
-            isError = showError,
-            modifier = modifier,
-            singleLine = true,
-            onValueChange = { onTextChanged(it) },
-            label = { Text(text = stringResource(id = hint)) },
-            keyboardOptions = keyboardOptions,
-            trailingIcon = trailingIcon
-        )
-
-        if (showError)
-        {
-            Text(
-                modifier = Modifier.offset(16.dp, 0.dp),
-                style = MaterialTheme.typography.caption,
-                text = error,
-                color = MaterialTheme.colors.error
-            )
-        }
-    }
-}
-
-@Composable
 fun LoginRegister(viewModel: LoginViewModel)
 {
     //todo should i pass the uiState only instead of the view model?
@@ -215,23 +116,22 @@ fun LoginRegister(viewModel: LoginViewModel)
         val maxWidthModifier = Modifier.fillMaxWidth()
 
         //email field
-        EmailPasswordTextField(
+        OutlinesTextFieldWithError(
             text = uiState.emailText,
             error = uiState.emailError,
             hint = R.string.hint_email,
-            keyboardOptions = KeyboardOptions.Default,
-            visualTransformation = VisualTransformation.None,
             modifier = maxWidthModifier,
-            trailingIcon = null,
             onTextChanged = { viewModel.onEmailInputChanged(it) }
         )
 
         Spacer(modifier = Modifier.height(5.dp))
 
-        PasswordField(
-            state = uiState,
-            modifier = maxWidthModifier
-        ) { viewModel.onPasswordInputChanged(it) }
+        PasswordTextField(
+            text = uiState.passwordText,
+            error = uiState.passwordError,
+            modifier = maxWidthModifier,
+            onTextChanged = { viewModel.onPasswordInputChanged(it) }
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
---
+++ app/src/main/java/com/hotmail/or_dvir/televiziacompose/ui/shared/SharedCompsables.kt
@@ -0,0 +1,124 @@
+package com.hotmail.or_dvir.televiziacompose.ui.shared
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.hotmail.or_dvir.televiziacompose.R
+import com.hotmail.or_dvir.televiziacompose.ui.login_register.LoginViewModel
+
+@Composable
+fun OutlinesTextFieldWithError(
+    text: String,
+    error: String,
+    @StringRes hint: Int,
+    modifier: Modifier = Modifier,
+    singleLine: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    onTextChanged: (String) -> Unit
+)
+{
+    val showError = error.isNotBlank()
+
+    Column {
+        OutlinedTextField(
+            visualTransformation = visualTransformation,
+            value = text,
+            isError = showError,
+            modifier = modifier,
+            singleLine = singleLine,
+            onValueChange = { onTextChanged(it) },
+            label = { Text(text = stringResource(id = hint)) },
+            keyboardOptions = keyboardOptions,
+            trailingIcon = trailingIcon
+        )
+
+        if (showError)
+        {
+            Text(
+                modifier = Modifier.offset(16.dp, 0.dp),
+                style = MaterialTheme.typography.caption,
+                text = error,
+                color = MaterialTheme.colors.error
+            )
+        }
+    }
+}
+
+@Composable
+fun PasswordTextField(
+    text: String,
+    error: String,
+    modifier: Modifier = Modifier,
+    @StringRes hint: Int = R.string.hint_password,
+    onTextChanged: (String) -> Unit
+)
+{
+    //todo this will not keep for configuration changes
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    val passwordTransformation: VisualTransformation
+    @DrawableRes val passwordIconId: Int
+    @StringRes val passwordContentDescriptionId: Int
+
+    if (isPasswordVisible)
+    {
+        passwordIconId = R.drawable.ic_visibility_off
+        passwordContentDescriptionId = R.string.contentDescription_hidePassword
+        passwordTransformation = VisualTransformation.None
+    } else
+    {
+        //password is hidden
+        passwordIconId = R.drawable.ic_visibility_on
+        passwordContentDescriptionId = R.string.contentDescription_showPassword
+        passwordTransformation = PasswordVisualTransformation()
+    }
+
+    OutlinesTextFieldWithError(
+        text = text,
+        error = error,
+        hint = hint,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = passwordTransformation,
+        modifier = modifier,
+        onTextChanged = onTextChanged,
+        trailingIcon = {
+            IconButton(
+                onClick = { isPasswordVisible = !isPasswordVisible }
+            ) {
+                Icon(
+                    painter = painterResource(passwordIconId),
+                    contentDescription = stringResource(passwordContentDescriptionId)
+                )
+            }
+        }
+    )
+}
